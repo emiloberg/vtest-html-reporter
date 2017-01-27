7ef95ee47042f06c6f5d67bf2a2a18fbcb8386df
Emil Öberg <emil.oberg@monator.com>
2017-01-27 10:18:44 +0100

    New way of getting config

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ function reportHTML({ specs, config, reporterConfig, log, chalk }) {
   const normalizedOutputPath = path.dirname(reporterConfig.output)
 
   const paths = {
-    new: path.relative(normalizedOutputPath, config.temp.screenshots.new),
-    master: path.relative(normalizedOutputPath, config.temp.screenshots.master),
-    diff: path.relative(normalizedOutputPath, config.temp.screenshots.diff)
+    new: path.relative(normalizedOutputPath, config.get("temp.screenshots.new")),
+    master: path.relative(normalizedOutputPath, config.get("temp.screenshots.master")),
+    diff: path.relative(normalizedOutputPath, config.get("temp.screenshots.diff"))
   }
 
   const source = fs.readFileSync(path.join(__dirname, "report-html.hbs"), "utf-8")
   const template = Handlebars.compile(source)
-  const html = template({ config, specs, paths })
+  const html = template({ config: config.getAll(), specs, paths })
 
   fs.outputFileSync(reporterConfig.output, html)
 
